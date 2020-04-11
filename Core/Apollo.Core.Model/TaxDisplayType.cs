c5f691ffd736aa06b4eb1032d00ff3e873495fa5
Han Chee <hancheester@gmail.com>
2020-04-11 19:11:02 +0100

    Cleanup

--- Core/Apollo.Core.Model/TaxDisplayType.cs
+++ Core/Apollo.Core.Model/TaxDisplayType.cs
@@ -2,13 +2,7 @@
 {
     public enum TaxDisplayType
     {
-        /// <summary>
-        /// Including tax
-        /// </summary>
         IncludingTax = 0,
-        /// <summary>
-        /// Excluding tax
-        /// </summary>
         ExcludingTax = 10,
     }
 }
